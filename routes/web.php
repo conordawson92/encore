a4f8fb0da589baeaec56827583a0eeb12d083131
Arthur <louis.arthur.michel@gmail.com>
2023-08-22 11:22:14 +0200

    added test logo, creation of layout, added route for test layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Test Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your tests. These routes
+| will be deleted once the tests are done. Be sure you do not put something
+| used in the final project in here. Put it above in the web routes section.
+|
+*/
+
+Route::get('/layout', function () {
+    return view('components/layout');
+});
