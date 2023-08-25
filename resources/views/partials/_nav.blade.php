dae0e6b3a983476ae00c0801dcfb017b34d696f0
Conor Dawson <conordawson92@googlemail.com>
2023-08-25 09:26:12 +0200

    Change to NAV

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -2,38 +2,33 @@
         <nav class="flex justify-between items-center bg-beige_logo text-brown_logo">
             <ul class="hidden md:flex space-x-6 text-lg m-auto">
                 @foreach ($parentCategories as $parentCategory)
-                    <li class="hover:bg-beige_logo_hover p-2">
-                        <a class="text-2xl" href="{{ route('showItemsByParentCategory', ['parentCategory' => $parentCategory->id]) }}">
-                            {{ $parentCategory->parentcategoryName }}
-                        </a>
-                    </li>
+                <li class="hover:bg-beige_logo_hover p-2">
+                    <a class="text-2xl" href="{{ route('showItemsByParentCategory', ['parentCategory' => $parentCategory->id]) }}">
+                        {{ $parentCategory->parentcategoryName }}
+                    </a>
+                </li>
+                @if($parentCategory->categories->count())
+                <div class="sub_menu absolute top-full left-0 group-hover:flex bg-beige_logo w-full w-max shadow-2xl">
+
+                </div>
+                @endif
+                </li>
                 @endforeach
                 <li class="hover:bg-beige_logo_hover p-2">
                     <a class="text-2xl" href="/about">
                         Our Mission
                     </a>
-                    @if($parentCategory->categories->count())
-                        <div class="sub_menu absolute top-full left-0 group-hover:flex bg-beige_logo w-full w-max shadow-2xl">
-    
-                        </div>
-                    @endif
                 </li>
-        @endforeach
-        <li class="hover:bg-beige_logo_hover p-2">
-            <a class="text-2xl" href="/about">
-                Our Mission
-            </a>
-        </li>
-        <li class="hover:bg-beige_logo_hover p-2">
-            <a class="text-2xl" href="/platform">
-                Our Platform
-            </a>
-        </li>
-    </ul>
-</nav>
-<!-- End of Desktop Navigation -->
-<!-- Start of Mobile Navigation -->
-<div id="menu_mobile_container">
-    
-</div>
-<!-- End of Mobile Navigation -->
+                <li class="hover:bg-beige_logo_hover p-2">
+                    <a class="text-2xl" href="/platform">
+                        Our Platform
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- End of Desktop Navigation -->
+        <!-- Start of Mobile Navigation -->
+        <div id="menu_mobile_container">
+
+        </div>
+        <!-- End of Mobile Navigation -->
