d7f371f741207ba84605c911479b0f99e568fa46
Cris <bariscristian126@gmail.com>
2023-08-28 16:18:12 +0200

    Shop update

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -2,18 +2,20 @@
 
 @props(['listing'])
 <x-card>
-    <div class="bg-white shadow-lg rounded-lg">
+    <div class="bg-white">
         <!-- Image -->
-        <img src="{{$listing->itemImage ? asset('' . $listing->itemImage): asset('images/no-image.png')}}" class="w-full h-60 object-cover rounded-t-lg" alt="...">
+        <div class="relative w-full h-36 sm:h-48">
+            <img src="{{$listing->itemImage ? asset('' . $listing->itemImage): asset('images/no-image.png')}}" class="absolute top-0 left-0 w-full h-full object-cover" alt="...">
+        </div>
         <!-- Information -->
-        <div class="p-4">
-            <h3 class="text-2xl">
+        <div class="p-2 sm:p-4">
+            <h3 class="text-xl sm:text-2xl">
                 <a href="/listings/{{$listing->id}}" class="text-black hover:text-gray-700">{{ $listing->ItemName }}</a>
             </h3>
-            <div class="text-xl font-bold mb-4">{{ $listing->description }}</div>
-            <div class="text-xl font-bold mb-4">€{{ $listing->price }}</div>
-            <div class="text-xl font-bold mb-4">Size: {{ $listing->size }}</div>
-            <div class="text-xl font-bold mb-4">Brand: {{ $listing->brand }}</div>
+            <div class="text-lg sm:text-xl font-bold mb-2 sm:mb-4">{{ $listing->description }}</div>
+            <div class="text-lg sm:text-xl font-bold mb-2 sm:mb-4">€{{ $listing->price }}</div>
+            {{-- <div class="text-lg sm:text-xl font-bold mb-2 sm:mb-4">Size: {{ $listing->size }}</div>
+            <div class="text-lg sm:text-xl font-bold mb-2 sm:mb-4">Brand: {{ $listing->brand }}</div> --}}
         </div>
     </div>
 </x-card>
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,21 +1,19 @@
 <x-layout>
+    <div class="container mx-auto">
 
-    <div class="container">
-        <div class="row row-cols-1 row-cols-md-2 g-4 mx-2">
-            @if (count($listings) == 0)
-                <p>No listing found</p>
-            @endif
-            
-            @foreach ($listings as $listing)
-                <div class="col">
-                    <x-listing-card :listing="$listing"/>
-                </div>
-            @endforeach
+        <!-- Listing Grid -->
+        <div class="flex justify-center">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full md:w-4/5">
+                @if (count($listings) == 0)
+                    <p>No listing found</p>
+                @endif
+
+                @foreach ($listings as $listing)
+                    <div>
+                        <x-listing-card :listing="$listing" />
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
-
-    {{-- <div class="mt-6 p-4">
-        {{$listings->links()}}
-    </div> --}}
-
 </x-layout>
