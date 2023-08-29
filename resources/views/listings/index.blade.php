4eff47cf841a16f122a66c10e30c61da1314846a
Cris <bariscristian126@gmail.com>
2023-08-29 09:34:40 +0200

    Conor Change on filter

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
 
-    <div class="flex flex-col md:flex-row">
+     <div class="flex flex-col md:flex-row">
 
         <!-- Sidebar with sort filters for desktop -->
         <div class="md:w-1/4 md:mr-4 mb-4 md:mb-0 p-2 bg-gray-100 rounded">
             <div class="text-center md:text-left mb-2 font-bold text-lg">
                 Sort by:
             </div>
+
             <div class="flex flex-wrap -m-2">
                 <div class="p-2 w-1/2 md:w-full">
                     <a href="?search={{ request('search') }}&sort=priceAsc" class="block bg-orange-500 text-white rounded px-4 py-2 hover:bg-orange-600 w-full text-center">Price (Low to High)</a>
@@ -21,14 +22,8 @@
                     <a href="?search={{ request('search') }}&sort=oldest" class="block bg-orange-500 text-white rounded px-4 py-2 hover:bg-orange-600 w-full text-center">Oldest</a>
                 </div>
             </div>
-        </div>
 
-        <!-- Listing Grid -->
-        <div class="flex justify-center md:w-3/4">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full">
-                @if (count($listings) == 0)
-                    <p>No listing found</p>
-                @endif
+        </div>
 
     <!-- Listing Grid -->
     <div class="flex justify-center">
@@ -50,6 +45,4 @@
         {{$listings->appends(['search' => request('search'), 'tag' => request('tag'), 'sort' => request('sort')])->links()}}
     </div>
 </x-layout>
-
-
 
