f4421f87ca32088449e997572bcbeafd9f741023
Cris <bariscristian126@gmail.com>
2023-08-29 15:25:08 +0200

    Listings update

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,4 @@ import "./search_mobile";
 import "./create_item";
 import "./dashboard_user-details";
 import "./image_overlay";
+
--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -5,7 +5,7 @@
     $tags = explode(',', $tagsCsv);
 @endphp
 
-<ul class="flex justify-center sm:justify-start flex-wrap">
+<ul class="flex justify-start sm:justify-start flex-wrap">
     {{--Display all the tags--}}
     @foreach ($tags as $tag)
     <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs ">
