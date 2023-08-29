3cd7596872713f6eeb607f64542f645884ebd2da
Cris <bariscristian126@gmail.com>
2023-08-29 09:35:05 +0200

    Conor Change sort

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -24,7 +24,7 @@
             </div>
 
         </div>
-
+        
     <!-- Listing Grid -->
     <div class="flex justify-center">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full md:w-4/5">
