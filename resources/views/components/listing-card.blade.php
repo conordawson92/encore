55087325efed730c19227dafbd32706d2dfe961f
Cris <bariscristian126@gmail.com>
2023-08-30 10:01:00 +0200

    Listings page change

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,15 +1,17 @@
 {{--Convert the list to an array--}}
-<head>
-    <title>Encore | Item</title>
-</head>
 
 @props(['listing'])
 <div class="w-full">
     <!-- Image -->
-    <div class="relative w-full h-96 overflow-hidden">
-        <img src="{{$listing->itemImage ? asset('' . $listing->itemImage): asset('images/no-image.png')}}" class="absolute top-0 left-0 w-full h-full object-cover filter blur-lg" alt="{{ $listing->ItemName }} Background Image">
-        <img src="{{$listing->itemImage ? asset('' . $listing->itemImage): asset('images/no-image.png')}}" class="absolute top-0 left-0 w-full h-full object-contain" alt="{{ $listing->ItemName }} Image">
-    </div>
+
+
+
+
+
+        <div class="relative w-full h-96 overflow-hidden">
+            <img src="{{$listing->itemImage ? asset('' . $listing->itemImage): asset('images/no-image.png')}}" class="absolute top-0 left-0 w-full h-full object-cover filter blur-lg" alt="{{ $listing->ItemName }} Background Image">
+            <img src="{{$listing->itemImage ? asset('' . $listing->itemImage): asset('images/no-image.png')}}" class="absolute top-0 left-0 w-full h-full object-contain" alt="{{ $listing->ItemName }} Image">
+      </div>
 
     <!-- Information -->
     <div class="p-4">
@@ -21,9 +23,9 @@
         </div>
         <div class="flex justify-end">
             <!-- Moved the heart button above the form -->
-            <button id="heartButton" class="text-red-500 hover:text-red-600 text-3xl mr-4">
-                <i class="far fa-heart"></i>  
-                <i class="fas fa-heart hidden"></i>  
+            <button id="heartButton" class="text-red-500 hover:text-red-600 text-3xl px-4 py-2">
+                <i class="far fa-heart mt-[-14px]"></i>  
+                <i class="fas fa-heart hidden mt-[-14px]"></i>  
             </button>
 
             <form action="{{ route('cart.add', $listing->id) }}" method="post">
@@ -32,34 +34,27 @@
                     Add to Cart
                 </button>
             </form>
-
-            {{-- <form action="{{ route('wishlist.add', $listing->id) }}" method="post">
-                @csrf
-                <button type="submit" class="ml-2 bg-blue-500 text-white py-1 px-4 rounded-none hover:bg-blue-600">
-                    Add Wishlist
-                </button>
-            </form> --}}
-
-            <form action="{{ route('wishlist.add', $listing->id) }}" method="post">
-                @csrf
-                <button type="submit" id="heartButton" class="text-red-500 hover:text-red-600 text-3xl mr-4">
-                    <i class="far fa-heart"></i>  
-                    <i class="fas fa-heart hidden"></i>  
-                </button>
-            </form>
         </div>
     </div>
 </div>
+
 <script>
     const heartButton = document.getElementById('heartButton');
 
     heartButton.addEventListener('click', function() {
         const heartEmpty = this.querySelector('.far');
         const heartFull = this.querySelector('.fas');
+
         heartEmpty.classList.toggle('hidden');
         heartFull.classList.toggle('hidden');
     });
 </script>
+
+
+
+
+
+
 
     
 
